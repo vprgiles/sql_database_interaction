0650513211f178a84a33467bf93c693f1355c19b
Vincent Giles <vprgiles.48265@gmail.com>
2025-07-30 18:49:55 +0100

    Linting auto-processed with black

--- functions/pyodbc_functions.py
+++ functions/pyodbc_functions.py
@@ -1,5 +1,5 @@
 import pyodbc
-from typing import Any, Literal, List, Dict, Tuple
+from typing import List
 import decimal
 from datetime import datetime
 import pandas as pd
@@ -22,21 +22,25 @@ def close_resources(cursor, connection, mode: int = 0):
         str or None
             Returns a string describing the error if one occurs during execution, otherwise returns None after resource closure.
     """
-    function_dict = {0: lambda: None,
-                     1: lambda: cursor.close(),
-                     2: lambda: (cursor.close(), connection.close())}
+    function_dict = {
+        0: lambda: None,
+        1: lambda: cursor.close(),
+        2: lambda: (cursor.close(), connection.close()),
+    }
     try:
         function_dict[mode]()
         return None
     except Exception as e:
-        return f'There was an error: {e}'
+        return f"There was an error: {e}"
 
 
-def server_connection(server_name: str,
-                      creds: list,
-                      database_name: str = 'master',
-                      driver: str = '{ODBC Driver 17 for SQL Server}',
-                      autocommit=False):
+def server_connection(
+    server_name: str,
+    creds: List,
+    database_name: str = "master",
+    driver: str = "{ODBC Driver 17 for SQL Server}",
+    autocommit=False,
+):
     """
     Creates a new SQL Server database with the specified name if it doesn't already exist.
 
@@ -63,24 +67,32 @@ def server_connection(server_name: str,
             return TypeError("Credentials list with 2 elements: [username, password]")
 
         connection = pyodbc.connect(
-                                     f"DRIVER={driver};"
-                                     f"SERVER={server_name};"
-                                     f"DATABASE={database_name};"
-                                     f"UID={creds[0]};"
-                                     f"PWD={creds[1]};",
-                                     autocommit=autocommit
+            f"DRIVER={driver};"
+            f"SERVER={server_name};"
+            f"DATABASE={database_name};"
+            f"UID={creds[0]};"
+            f"PWD={creds[1]};",
+            autocommit=autocommit,
         )
         cursor = connection.cursor()
-        print(f"Successfully connected to server: {connection.getinfo(pyodbc.SQL_SERVER_NAME)}")
-        print(f"Connected to database: {cursor.execute("SELECT DB_NAME()").fetchall()[0][0]}")
+        print(
+            f"Successfully connected to server: {connection.getinfo(pyodbc.SQL_SERVER_NAME)}"
+        )
+        print(
+            f"Connected to database: {cursor.execute("SELECT DB_NAME()").fetchall()[0][0]}"
+        )
     except Exception as e:
         return e
 
     return connection, cursor
 
 
-def create_new_database(database_name: str, cursor: pyodbc.Cursor,
-                        connection: pyodbc.Connection, close_connection: int = 0):
+def create_new_database(
+    database_name: str,
+    cursor: pyodbc.Cursor,
+    connection: pyodbc.Connection,
+    close_connection: int = 0,
+):
     """
     Creates a new SQL Server database with the specified name if it doesn't already exist.
 
@@ -106,22 +118,34 @@ def create_new_database(database_name: str, cursor: pyodbc.Cursor,
         Exception: If an error occurs during execution, the exception is returned.
     """
     try:
-        database_present = cursor.execute(f"SELECT name FROM sys.databases WHERE name = '{database_name}'").fetchall()
+        database_present = cursor.execute(
+            f"SELECT name FROM sys.databases WHERE name = '{database_name}'"
+        ).fetchall()
         if database_present == []:
             cursor.execute(f"CREATE DATABASE [{database_name}]")
             connection.commit()
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
         else:
-            print(f"There is already a database with the name: {database_name} " +
-                  f"in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}")
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            print(
+                f"There is already a database with the name: {database_name} "
+                + f"in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}"
+            )
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
 
     except Exception as e:
         return e
 
 
-def generate_template_table(table_name: str, cursor: pyodbc.Cursor,
-                            connection: pyodbc.Connection, close_connection: int = 0):
+def generate_template_table(
+    table_name: str,
+    cursor: pyodbc.Cursor,
+    connection: pyodbc.Connection,
+    close_connection: int = 0,
+):
     """Creates a generic template SQL table from a pre-defined (hardcoded) schema. Use to verify functionality.
 
     Parameters
@@ -143,12 +167,15 @@ def generate_template_table(table_name: str, cursor: pyodbc.Cursor,
             otherwise returns None after successful table creation.
     """
     try:
-        table_present = cursor.execute(f""" SELECT TABLE_NAME
+        table_present = cursor.execute(
+            f""" SELECT TABLE_NAME
                                             FROM INFORMATION_SCHEMA.TABLES
                                             WHERE TABLE_TYPE='BASE TABLE'
-                                            AND TABLE_NAME='{table_name}'""").fetchall()
+                                            AND TABLE_NAME='{table_name}'"""
+        ).fetchall()
         if table_present == []:
-            cursor.execute(f'''
+            cursor.execute(
+                f"""
             CREATE TABLE {table_name} (
                     PersonId INTEGER PRIMARY KEY,
                     FirstName NVARCHAR NOT NULL,
@@ -156,19 +183,28 @@ def generate_template_table(table_name: str, cursor: pyodbc.Cursor,
                     Age INTEGER NULL,
                     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
                     );
-                    ''')
+                    """
+            )
             connection.commit()
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
         else:
-            print(f"There is already a table with the name: {table_name} " +
-                  f"in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}")
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            print(
+                f"There is already a table with the name: {table_name} "
+                + f"in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}"
+            )
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
 
     except Exception as e:
-        return f'There was an error: {e}'
+        return f"There was an error: {e}"
 
 
-def generate_schema_from_df(dataframe: pd.DataFrame, limit_string_search: int = 0) -> str:
+def generate_schema_from_df(
+    dataframe: pd.DataFrame, limit_string_search: int = 0
+) -> str:
     """
     Generates a SQL table schema from a pandas DataFrame by mapping column data types
     to SQL-compatible types.
@@ -188,25 +224,26 @@ def generate_schema_from_df(dataframe: pd.DataFrame, limit_string_search: int = 
             inferred data types.
     """
     type_mapping = {
-        'int64': 'BIGINT',
-        'int32': 'INT',
-        'int16': 'SMALLINT',
-        'float64': 'FLOAT',
-        'float32': 'REAL',
-        'bool': 'BIT',
-        'object': 'TEXT',
-        'datetime64[ns]': 'TIMESTAMP'}
+        "int64": "BIGINT",
+        "int32": "INT",
+        "int16": "SMALLINT",
+        "float64": "FLOAT",
+        "float32": "REAL",
+        "bool": "BIT",
+        "object": "TEXT",
+        "datetime64[ns]": "TIMESTAMP",
+    }
     schema = ""
     column_definitions = []
     for col in dataframe.columns:
         data_type = dataframe[col].dtype
 
-        if data_type == 'object':  # Treat objects separately
+        if data_type == "object":  # Treat objects separately
             if not dataframe[col].dropna().empty:
                 data_sample = dataframe[col].dropna().iloc[0]
 
                 if isinstance(data_sample, bytes) or isinstance(data_sample, bytearray):
-                    sql_type = 'VARBINARY(MAX)'
+                    sql_type = "VARBINARY(MAX)"
 
                 elif isinstance(data_sample, str):
                     if limit_string_search > 0:
@@ -214,31 +251,31 @@ def generate_schema_from_df(dataframe: pd.DataFrame, limit_string_search: int = 
                     else:
                         data = dataframe[col]
                     if max(len(str(value)) for value in data) < 255:
-                        sql_type = 'VARCHAR(255)'
+                        sql_type = "VARCHAR(255)"
                     else:
-                        sql_type = 'TEXT'
+                        sql_type = "TEXT"
 
                 elif isinstance(data_sample, decimal.Decimal):
                     sign, digits, exponent = data_sample.as_tuple()
                     scale = -int(exponent) if int(exponent) < 0 else 0
 
-                    if set(str(data_sample).split('.')[0].lstrip('-')) == set('0'):
+                    if set(str(data_sample).split(".")[0].lstrip("-")) == set("0"):
                         # Only zeros before decimal point
-                        precision = len(str(data_sample).split('.')[1])
+                        precision = len(str(data_sample).split(".")[1])
                     else:
                         # Non-zeros before decimal point
-                        precision = len(str(data_sample).lstrip('-').replace('.', ''))
+                        precision = len(str(data_sample).lstrip("-").replace(".", ""))
 
-                    sql_type = f'DECIMAL({precision},{scale})'
+                    sql_type = f"DECIMAL({precision},{scale})"
 
                 elif isinstance(data_sample, datetime):
-                    sql_type = 'TIMESTAMP'
+                    sql_type = "TIMESTAMP"
                 else:
-                    sql_type = 'TEXT'  # Assign when the datatype is not one of the written options
+                    sql_type = "TEXT"  # Assign when the datatype is not one of the written options
             else:
-                sql_type = 'TEXT'  # Assign when the dataframe column is empty
+                sql_type = "TEXT"  # Assign when the dataframe column is empty
         else:
-            sql_type = type_mapping.get(str(data_type), 'TEXT')
+            sql_type = type_mapping.get(str(data_type), "TEXT")
 
         column_definitions.append(f" {col} {sql_type}")
 
@@ -247,8 +284,13 @@ def generate_schema_from_df(dataframe: pd.DataFrame, limit_string_search: int = 
     return schema
 
 
-def create_table_from_schema(schema: str, table_name: str, cursor: pyodbc.Cursor,
-                             connection: pyodbc.Connection, close_connection: int = 0):
+def create_table_from_schema(
+    schema: str,
+    table_name: str,
+    cursor: pyodbc.Cursor,
+    connection: pyodbc.Connection,
+    close_connection: int = 0,
+):
     """
     Creates a SQL table from a schema in text form.
 
@@ -273,30 +315,43 @@ def create_table_from_schema(schema: str, table_name: str, cursor: pyodbc.Cursor
             otherwise returns None after successful table creation.
     """
     try:
-        table_present = cursor.execute(f""" SELECT TABLE_NAME
+        table_present = cursor.execute(
+            f""" SELECT TABLE_NAME
                                             FROM INFORMATION_SCHEMA.TABLES
                                             WHERE TABLE_TYPE='BASE TABLE'
-                                            AND TABLE_NAME='{table_name}'""").fetchall()
+                                            AND TABLE_NAME='{table_name}'"""
+        ).fetchall()
         if table_present == []:  # No table with that name
-            cursor.execute(f'''
+            cursor.execute(
+                f"""
                 CREATE TABLE {table_name} (
                         {schema}
                         );
-                        ''')
+                        """
+            )
             connection.commit()
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
         else:
-            already_table_message = (f"There is already a table with the name: {table_name}") + \
-                                     (f" in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}")
-            close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+            already_table_message = (
+                f"There is already a table with the name: {table_name}"
+            ) + (f" in {connection.getinfo(pyodbc.SQL_SERVER_NAME)}")
+            close_resources(
+                cursor, connection, mode=close_connection
+            )  # Choose to close the connection
             return already_table_message
 
     except Exception as e:
-        return f'There was an error: {e}'
+        return f"There was an error: {e}"
 
 
-def return_table_schema(table_name: str, cursor: pyodbc.Cursor,
-                        connection: pyodbc.Connection, close_connection: int = 0):
+def return_table_schema(
+    table_name: str,
+    cursor: pyodbc.Cursor,
+    connection: pyodbc.Connection,
+    close_connection: int = 0,
+):
     """
     Generates and returns the SQL column definitions for a specified table in a pyodbc-supported database.
 
@@ -322,25 +377,35 @@ def return_table_schema(table_name: str, cursor: pyodbc.Cursor,
     schema_lines = []
     try:
         for col in cursor.columns(table=table_name):
-            if col.type_name.upper() == 'VARCHAR':
-                schema_lines.append(f" {col.column_name} {(col.type_name).upper()}({col.column_size})")
+            if col.type_name.upper() == "VARCHAR":
+                schema_lines.append(
+                    f" {col.column_name} {(col.type_name).upper()}({col.column_size})"
+                )
 
-            elif col.type_name.upper() == 'DECIMAL':
-                schema_lines.append(f" {col.column_name} {(col.type_name).upper()}({col.column_size},{col.decimal_digits})")
+            elif col.type_name.upper() == "DECIMAL":
+                schema_lines.append(
+                    f" {col.column_name} {(col.type_name).upper()}({col.column_size},{col.decimal_digits})"
+                )
 
-            elif col.type_name.upper() == 'VARBINARY':
+            elif col.type_name.upper() == "VARBINARY":
                 if col.column_size == 0:
-                    col_size = 'MAX'
-                    schema_lines.append(f" {col.column_name} {(col.type_name).upper()}({col_size})")
+                    col_size = "MAX"
+                    schema_lines.append(
+                        f" {col.column_name} {(col.type_name).upper()}({col_size})"
+                    )
                 else:
-                    schema_lines.append(f" {col.column_name} {(col.type_name).upper()}({col.column_size})")
+                    schema_lines.append(
+                        f" {col.column_name} {(col.type_name).upper()}({col.column_size})"
+                    )
 
             else:
                 schema_lines.append(f" {col.column_name} {(col.type_name).upper()}")
 
         schema += ",\n".join(schema_lines)
-        close_resources(cursor, connection, mode=close_connection)  # Choose to close the connection
+        close_resources(
+            cursor, connection, mode=close_connection
+        )  # Choose to close the connection
         return schema
 
     except Exception as e:
-        return f'There was an error: {e}'
+        return f"There was an error: {e}"
